642a114e7025422da0777e75d55652263310fc8e
Skalv <fboutin76@gmail.com>
2016-10-27 10:08:50 +0200

    Création d'une interface simple

--- app/app.js
+++ app/app.js
@@ -4,33 +4,68 @@ const Item   = require('./models/item');
 const Fight  = require('./models/fight');
 const Arena  = require('./models/arena');
 
-// Création des Joueurs
-var joueur1 = new Player('Florent', "fboutin76@gmail.com", "tutu");
-var joueur2 = new Player('Albert', 'albert2000&gmail.com', "titi");
+// Création des variables globales.
+var joueur1;
 
-// Création d'une team
-var team1 = new Team('webforce');
+$('.formPlayer').on('submit', function(e) {
+  // Stop l'envois du formulaire vers le serveur.
+  e.preventDefault();
+  // Récup des datas.
+  var datas = $(this).serializeArray();
+  // Formate les données dans un objet JS.
+  var formatdata = {};
+  for (var i = 0; i < datas.length; i++) {
+    formatdata[datas[i].name] = datas[i].value;
+  }
+  // Création du Player.
+  joueur1 = new Player(formatdata.username, formatdata.email, formatdata.password);
+  // Cacher le formulaire
+  $('.formPlayer').addClass('hide');
+  // Ajout du nom du joueur dans le titre de la page.
+  $('.pageTitle').text("Salut " + formatdata.username);
+  // Afficher le formulaire de création de personnage.
+  $('.formCharactere').removeClass('hide');
+})
 
-// Création de l'arene
-var arena = new Arena('Kind');
+$('.formCharactere').on('submit', function(e) {
+  // Stop l'envois du formulaire vers le serveur.
+  e.preventDefault();
+  // Récup des datas.
+  var datas = $(this).serializeArray();
+  // Formate les données dans un objet JS.
+  var formatdata = {};
+  for (var i = 0; i < datas.length; i++) {
+    formatdata[datas[i].name] = datas[i].value;
+  }
+  // Création du personnage.
+  joueur1.createCharactere(formatdata.name, formatdata.classe, formatdata.race);
+  // Si nous avons deux personnages, alors on affiche le bouton de Combat
+  // Sinon on vide le formulaire pour que le joueur puisse créer un autre personnage.
+  if (joueur1.characteres.length === 2) {
+    // Cacher le formulaire de création de personnage
+    $('.formCharactere').addClass('hide');
+    // Afficher le bouton de combat
+    $('.startFight').removeClass('hide');
+  } else {
+    // ajout d'une notification au joueur
+    $('.notification').text('Il faut second personnage pour combattre !');
+    // Vide le formulaire
+    $('.formCharactere input[type=text]').val('');
+  }
+})
 
-// Création d'équipements
-var item1 = new Item('bottes', 'Bottes en cuir d\'ascalon', 10, 30, 'Bottes en cuir de Charr', 50);
-var item2 = new Item('torse', 'Bottes en cuir d\'ascalon', 30, 100, 'Bottes en cuir de Charr', 50);
+$('.startFight').on('click', function(e) {
+  e.preventDefault();
+  // Prévention au cas ou on lance le combat sans avoir deux personnages
+  if (joueur1.characteres.length !== 2) {
+    return;
+  }
+  // Création de l'arene
+  var arena = new Arena('Kind');
+  // On prépare le combat.
+  var fight1 = new Fight(joueur1.characteres[0], joueur1.characteres[1], arena);
+  fight1.prepareToFight();
+})
 
-
-// Ajout du joueur à la team.
-team1.addPlayer(joueur1);
-
-// Ajout d'un nouveau personnage à la collection de joueur1
-var monPerso = joueur1.createCharactere('Skalv', "fullstack Dev", "Barbu");
-var persoAlbert = joueur2.createCharactere('Bebert', "chercheur", "Random");
-
-
-// Ajout de nos bottes à notre personnage
-// monPerso.equip(item1);
-// persoAlbert.equip(item2);
-
-// Combat entre le joueur1 et le joueur2
-var fight1 = new Fight(monPerso, persoAlbert, arena);
-fight1.prepareToFight();
+// var fight1 = new Fight(monPerso, persoAlbert, arena);
+// fight1.prepareToFight();
